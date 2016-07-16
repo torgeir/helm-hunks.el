fcf88da76ee39ff2e65c5ce21b99a1c543d40237
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-17 00:54:35 +0200

    credit git-gutter+

--- helm-hunks.el
+++ helm-hunks.el
@@ -25,6 +25,8 @@
 ;;
 ;; Enable `helm-follow-mode' and trigger `helm-hunks' to jump around
 ;; unstaged hunks like never before.
+;;
+;; Credits/inspiration: git-gutter+ - https://github.com/nonsequitur/git-gutter-plus/
 
 ;;; Todo:
 
