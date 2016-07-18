7f2c49d48e3230f278c868baa2fc53a5f9cf5ccf
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-18 08:08:53 +0200

    melpa feedback: don't depend on helm v0

--- helm-hunks.el
+++ helm-hunks.el
@@ -5,7 +5,7 @@
 ;; Author: @torgeir
 ;; Version: 1.0.0
 ;; Keywords: helm git hunks vc
-;; Package-Requires: ((helm "0"))
+;; Package-Requires: ((helm "1.9.8"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
