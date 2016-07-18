b27ce4ced47cedb5db6a520d52bcd3f6272dda74
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-18 08:43:58 +0200

    melpa feedback: run checkdoc and fix docstrings

--- helm-hunks.el
+++ helm-hunks.el
@@ -42,53 +42,52 @@
 
 (defconst helm-hunks--diff-re
   "^@@ -\\([0-9]+\\)\\(?:,\\([0-9]+\\)\\)? \\+\\([0-9]+\\)\\(?:,\\([0-9]+\\)\\)? @@"
-  "Regex to match the git diff hunk lines, e.g `@@ -{del-line},{del-len} +{add-line},{add-len} @@'")
+  "Regex to match the git diff hunk lines, e.g `@@ -{del-line},{del-len} +{add-line},{add-len} @@'.")
 
 (defvar helm-hunks--cmd-file-names
   "git --no-pager diff --name-only"
-  "Git command to return names of the changed files")
+  "Git command to return names of the changed files.")
 
 (defvar helm-hunks--cmd-diffs
   "git --no-pager diff --no-color --no-ext-diff --unified=0"
-  "Git command to show minimal diffs")
+  "Git command to show minimal diffs.")
 
 (defvar helm-hunks--cmd-git-root
   "git rev-parse --show-toplevel"
-  "Git command to find the root folder of the current repo")
+  "Git command to find the root folder of the current repo.")
 
 (defvar helm-hunks--cmd-git-apply
   "git apply --unidiff-zero --cached -"
-  "Git command to apply the patch read from stdin")
+  "Git command to apply the patch read from stdin.")
 
 (defvar helm-hunks--msg-no-changes
   "No changes."
-  "Message shown in the helm buffer when there are no changed hunks")
+  "Message shown in the helm buffer when there are no changed hunks.")
 
 (defvar helm-hunks--is-preview
   nil
-  "Is preview mode enabled, to show diff lines preview inside helm while navigating")
+  "Is preview mode enabled, to show diff lines preview inside helm while navigating.")
 
 (defvar helm-hunks-refresh-hook
   nil
-  "Hooks triggered whenever helm-hunks trigger git changes, so you can refresh your
-favorite git-gutter on git changes")
+  "Hooks triggered whenever `helm-hunks' trigger git changes, so you can refresh your favorite git-gutter on git changes.")
 
 ;; Refresh git-gutter+ on git changes
 (when (fboundp 'git-gutter+-refresh)
   (add-hook 'helm-hunks-refresh-hook 'git-gutter+-refresh))
 
 (defun helm-hunks--take (n lst)
-  "Take `n' elements of `lst'"
+  "Take `N' elements of `LST'."
   (butlast lst (- (length lst) n)))
 
 (defun helm-hunks--get-file-names ()
-  "List file names of changed files"
+  "List file names of changed files."
   (let* ((result (shell-command-to-string helm-hunks--cmd-file-names))
          (raw-file-names (split-string result "\r?\n")))
     (delete "" raw-file-names)))
 
 (defun helm-hunks--get-diffs ()
-  "List raw diffs per changed file"
+  "List raw diffs per changed file."
   (let* ((result (shell-command-to-string helm-hunks--cmd-diffs))
          (split-diff-lines (split-string result "^diff --git a/"))
          (split-diff-lines-without-empties (delete "" split-diff-lines)))
@@ -97,19 +96,25 @@ favorite git-gutter on git changes")
             split-diff-lines-without-empties)))
 
 (defun helm-hunks--extract-hunk-lines (diff)
-  "Split on ^@@ to group lists of each hunk's header and content lines in a list"
+  "Split `DIFF' string on ^@@ to group lists of each hunk's header and content lines in a list."
   (mapcar (lambda (hunk)
             (concat "@@" hunk))
           (delete "" (split-string diff "^@@"))))
 
 (defun helm-hunks--get-git-root ()
-  "Get the root folder of the current git repository"
+  "Get the root folder of the current git repository."
   (let* ((result (shell-command-to-string helm-hunks--cmd-git-root)))
     (file-name-as-directory
      (replace-regexp-in-string "\r?\n" "" result))))
 
 (defun helm-hunks--parse-hunk (diff-header-str hunk-str)
-  "Parse `raw-hunk' into hunk with `line', `content' and the `type' of change"
+  "Join `DIFF-HEADER-STR' and the parsed `HUNK-STR' into a hunk.
+
+A hunk is an association list with the fields `diff-header' containing
+the diff header-line, `hunk-header' containing the hunks , `content'
+containing the content of the change, `raw-content' containing the raw
+diff content as an individual patch, `type' the type of change and
+`line' the line the change occured on."
   (let* ((hunk-lines (split-string hunk-str "\n"))
          (hunk-header-line (car hunk-lines))
          (content-lines (cdr hunk-lines)))
@@ -132,14 +137,18 @@ favorite git-gutter on git changes")
               (cons 'line line))))))
 
 (defun helm-hunks--assoc-file-name (file-name hunks)
-  "Associates `file' name with each hunk of the list."
+  "Associates `FILE-NAME' name with each hunk of the `HUNKS' list."
   (mapcar (lambda (hunk)
             (cons (cons 'file file-name)
                   hunk))
           hunks))
 
 (defun helm-hunks--get-hunks-by-file (file-names diffs-per-file)
-  "Join the changed file names with their corresponding hunks in a list"
+  "Join the changed file names with their corresponding hunks in a list.
+
+`FILE-NAMES' is the list of file names that changed.
+
+`DIFFS-PER-FILE' holds the diff hunks ordered per file name."
   (cl-loop for file-name in file-names
            for diff-str in diffs-per-file
            collect (let* ((split-hunk (split-string diff-str "\r?\n"))
@@ -154,7 +163,7 @@ favorite git-gutter on git changes")
                      (cons file-name parsed-hunks-with-file))))
 
 (defun helm-hunks--run-hooks-for-buffer-of-hunk (hunk)
-  "Runs refresh hooks with the buffer visiting the `hunk's file"
+  "Run refresh hooks with the buffer visiting the `HUNK's file."
   (let* ((path-of-hunk (cdr (assoc 'file hunk)))
          (buffer-name (file-name-nondirectory path-of-hunk)))
     (when buffer-name
@@ -162,7 +171,7 @@ favorite git-gutter on git changes")
         (run-hooks 'helm-hunks-refresh-hook)))))
 
 (defun helm-hunks--fontify-as-diff (content)
-  "Fontify content as a diff shown in `diff-mode'"
+  "Fontify `CONTENT' as a diff, like it's shown in `diff-mode'."
   (with-temp-buffer
     (insert content)
     (diff-mode)
@@ -170,46 +179,53 @@ favorite git-gutter on git changes")
     (font-lock-default-fontify-buffer)
     (buffer-string)))
 
-(defun helm-hunks--format-candidate-for-display (file hunk)
-  "Formats a hunk for display as a line in helm"
-  (unless (equal file helm-hunks--msg-no-changes)
-    (let* ((line (cdr (assoc 'line hunk)))
-           (type (cdr (assoc 'type hunk)))
-           (content (cdr (assoc 'content hunk)))
-           (is-content-empty (equal "" content)))
-      (if (and helm-hunks--is-preview
-               (not is-content-empty))
-          (helm-hunks--format-candidate-multiline file line type content)
-        (helm-hunks--format-candidate-line file line type)))))
+(defun helm-hunks--format-candidate-for-display (hunk)
+  "Formats `HUNK' for display as a line in helm."
+  (let ((file (cdr (assoc 'file hunk))))
+    (unless (equal file helm-hunks--msg-no-changes)
+      (let* ((line (cdr (assoc 'line hunk)))
+             (type (cdr (assoc 'type hunk)))
+             (content (cdr (assoc 'content hunk)))
+             (is-content-empty (equal "" content)))
+        (if (and helm-hunks--is-preview
+                 (not is-content-empty))
+            (helm-hunks--format-candidate-multiline file line type content)
+          (helm-hunks--format-candidate-line file line type))))))
 
 (defun helm-hunks--format-candidate-multiline (file line type content)
-  "Formats a multiline hunk, with `content' fontified as diff"
+  "Formats a multiline hunk, fontifying the contents of the diff.
+
+Includes the `FILE' the change occured in, the `LINE' the change
+occured at, the `TYPE' of change, and the `CONTENT' of the change."
   (let ((fontified-content (helm-hunks--fontify-as-diff content)))
     (format "%s:%s (%s)\n%s" file line type fontified-content)))
 
 (defun helm-hunks--format-candidate-line (file line type)
-  "Format a single line hunk"
+  "Format a single line hunk.
+
+Includes the `FILE' the change occured in, the `LINE' the change
+occured at and the `TYPE' of change."
   (format "%s:%s (%s)" file line type))
 
-(defun helm-hunks--find-hunk-with-fn (find-file-fn real)
-  "Jump to the changed line in the file using the provided `find-file-fn' function"
-  (let* ((file (cdr (assoc 'file real)))
-         (line (cdr (assoc 'line real)))
+(defun helm-hunks--find-hunk-with-fn (hunk find-file-fn)
+  "Jump to the changed line in the file of the `HUNK' using the provided `FIND-FILE-FN' function."
+  (let* ((file (cdr (assoc 'file hunk)))
+         (line (cdr (assoc 'line hunk)))
          (file-path (concat (helm-hunks--get-git-root) file)))
     (funcall find-file-fn file-path)
     (goto-char (point-min))
     (forward-line (1- line))))
 
-(defun helm-hunks--action-find-hunk-other-frame (real)
-  "Jump to the changed line in the file using `find-file-other-frame'"
-  (helm-hunks--find-hunk-with-fn #'find-file-other-frame real))
+(defun helm-hunks--action-find-hunk-other-frame (hunk)
+  "Jump to the changed line in the file of the `HUNK' using `find-file-other-frame'."
+  (helm-hunks--find-hunk-with-fn hunk #'find-file-other-frame))
 
-(defun helm-hunks--action-find-hunk-other-window (real)
-  "Jump to the changed line in the file using `find-file-other-window'"
-  (helm-hunks--find-hunk-with-fn #'find-file-other-window real))
+(defun helm-hunks--action-find-hunk-other-window (hunk)
+  "Jump to the changed line in the file of the `HUNK' using `find-file-other-window'."
+  (helm-hunks--find-hunk-with-fn hunk #'find-file-other-window))
 
 (defun helm-hunks--toggle-preview-interactive ()
-  "Toggle diff lines preview mode inside helm, while helm is open"
+  "Toggle diff lines preview mode inside helm, while helm is open."
   (interactive)
   (let* ((is-preview (not helm-hunks--is-preview))
          (hunk (helm-get-selection))
@@ -222,19 +238,19 @@ favorite git-gutter on git changes")
       (helm-force-update candidate))))
 
 (defun helm-hunks--find-hunk-other-frame-interactive ()
-  "Interactive defun to jump to the changed line in the file in another frame"
+  "Interactive defun to jump to the changed line in the file in another frame."
   (interactive)
   (with-helm-alive-p
     (helm-exit-and-execute-action #'helm-hunks--action-find-hunk-other-frame)))
 
 (defun helm-hunks--find-hunk-other-window-interactive ()
-  "Interactive defun to jump to the changed line in the file in another window"
+  "Interactive defun to jump to the changed line in the file in another window."
   (interactive)
   (with-helm-alive-p
     (helm-exit-and-execute-action #'helm-hunks--action-find-hunk-other-window)))
 
 (defun helm-hunks--stage-hunk-interactive ()
-  "Interactive defun to stage the currently selected helm candidate's hunk (`real' value)"
+  "Interactive defun to stage the currently selected helm candidate's hunk (`real' value)."
   (interactive)
   (with-helm-alive-p
     (let ((real (helm-get-selection)))
@@ -243,9 +259,11 @@ favorite git-gutter on git changes")
         (helm-refresh)
         (helm-hunks--run-hooks-for-buffer-of-hunk real)))))
 
-(defun helm-hunks--stage-hunk (real)
-  "Stage a hunk. Will `cd' to the git root to make git diff paths align with paths on disk as we're not nescessarily in the git root when helm-hunks is run, and diffs are gathered."
-  (let ((raw-hunk-diff (cdr (assoc 'raw-content real))))
+(defun helm-hunks--stage-hunk (hunk)
+  "Stage a `HUNK'.
+
+Will `cd' to the git root to make git diff paths align with paths on disk as we're not nescessarily in the git root when `helm-hunks' is run, and diffs are gathered."
+  (let ((raw-hunk-diff (cdr (assoc 'raw-content hunk))))
     (with-temp-buffer
       (insert raw-hunk-diff)
       (unless (zerop
@@ -259,7 +277,7 @@ favorite git-gutter on git changes")
         (buffer-string)))))
 
 (defun helm-hunks--changes ()
-  "List changes, on the form `(display . real)' suitable as candidates for the helm-hunks source"
+  "Create a list of candidates on the form `(display . real)' suitable for the `helm-hunks' source."
   (reverse
    (let* ((hunks-by-file (helm-hunks--get-hunks-by-file (helm-hunks--get-file-names)
                                                         (helm-hunks--get-diffs)))
@@ -268,25 +286,25 @@ favorite git-gutter on git changes")
        (let* ((file (car hunk-by-file))
               (hunks (cdr hunk-by-file)))
          (dolist (hunk hunks)
-           (push `(,(helm-hunks--format-candidate-for-display file hunk) . ,hunk)
+           (push `(,(helm-hunks--format-candidate-for-display hunk) . ,hunk)
                  changes)))))))
 
 (defun helm-hunks--candidates ()
-  "Candidates for the helm-hunks source, on the form (display . real)"
+  "Candidates for the `helm-hunks' source, on the form (display . real)."
   (let ((candidates (helm-hunks--changes)))
     (if (equal nil candidates)
         `((,helm-hunks--msg-no-changes . nil))
       candidates)))
 
-(defun helm-hunks--action-find-hunk (real)
-  "Action to trigger on RET, for the helm-hunks source"
-  (unless (equal real helm-hunks--msg-no-changes)
-    (helm-hunks--find-hunk-with-fn #'find-file real)))
+(defun helm-hunks--action-find-hunk (hunk)
+  "Action that triggers on RET for the `helm-hunks' source. Jumps to the file of the `HUNK'."
+  (unless (equal hunk helm-hunks--msg-no-changes)
+    (helm-hunks--find-hunk-with-fn hunk #'find-file)))
 
-(defun helm-hunks--persistent-action (real)
-  "Persistent action to trigger on follow, for the helm-hunks source"
-  (unless (equal real helm-hunks--msg-no-changes)
-    (helm-hunks--find-hunk-with-fn #'find-file real)))
+(defun helm-hunks--persistent-action (hunk)
+  "Persistent action to trigger on follow for the `helm-hunks' source. Jumps to the file of the `HUNK'."
+  (unless (equal hunk helm-hunks--msg-no-changes)
+    (helm-hunks--find-hunk-with-fn hunk #'find-file)))
 
 (defvar helm-hunks--source
   (helm-build-async-source "Show hunks in project"
@@ -296,7 +314,7 @@ favorite git-gutter on git changes")
     :multiline t
     :nomark t
     :follow 1)
-  "Helm-hunks source to list changed hunks in the project")
+  "Helm-hunks source to list changed hunks in the project.")
 
 (defvar helm-hunks--keymap
   (let ((map (make-sparse-keymap)))
@@ -308,11 +326,11 @@ favorite git-gutter on git changes")
     (define-key map (kbd "C-c o") 'helm-hunks--find-hunk-other-window-interactive)
     (define-key map (kbd "C-c p") 'helm-hunks--toggle-preview-interactive)
     map)
-  "Keymap for `helm-hunks'")
+  "Keymap for `helm-hunks'.")
 
 ;;;###autoload
 (defun helm-hunks ()
-  "Helm-hunks entry point"
+  "Helm-hunks entry point."
   (interactive)
   (helm :sources '(helm-hunks--source)
         :keymap helm-hunks--keymap))
